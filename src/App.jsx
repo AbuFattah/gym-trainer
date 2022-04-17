facc7fc773b4918991117a0b3bdb103e37d97826
AbuFattah <mdnahid142493@gmail.com>
2022-04-17 14:44:56 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,42 @@ import Banner from "./components/Banner";
 function App() {
   return (
     <>
-      <header>
-        <div className="container p-4 mx-auto">
-          <p>
-            <span>A</span>Trains
+      <div className="banner relative flex items-center justify-center text-white">
+        <header className="text-white absolute w-full top-0">
+          <div className="container py-5 px-10 mx-auto flex flex-col gap-10 md:flex-row justify-between items-center">
+            <p className="text-3xl font-semibold">
+              <span className="text-secondary font-bold">A</span>Trains
+            </p>
+            <nav>
+              <ul className="flex gap-10 text-xl">
+                <li>
+                  <Link to="services">Sevices</Link>
+                </li>
+                <li>
+                  <Link to="Contact">Contact</Link>
+                </li>
+                <li>
+                  <Link to="About">About</Link>
+                </li>
+                <li>
+                  <Link to="Blogs">Blogs</Link>
+                </li>
+              </ul>
+            </nav>
+            <button className="btn btn-ghost">Register</button>
+          </div>
+        </header>
+
+        <div className="text-center space-y-12">
+          <h1 className="hidden md:block text-5xl md:text-8xl font-bold italic">
+            PERSONAL TRAINING
+          </h1>
+          <p className="text-4xl text-accent">
+            Premium online personal training with Andrew Adams.
           </p>
-          <nav>
-            <ul>
-              <li>
-                <Link to="services">Sevices</Link>
-              </li>
-              <li>
-                <Link to="Contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="About">About</Link>
-              </li>
-              <li>
-                <Link to="Blogs">Blogs</Link>
-              </li>
-            </ul>
-          </nav>
+          <button className="btn btn-accent">START HERE</button>
         </div>
-      </header>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,30 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#02044A",
+
+          secondary: "#0C75FF",
+
+          accent: "#9ec4fb",
+
+          neutral: "#fff",
+
+          "base-100": "#f2f4f8",
+
+          info: "#33BCDB",
+
+          success: "#6FDC99",
+
+          warning: "#B17806",
+
+          error: "#EF554D",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
